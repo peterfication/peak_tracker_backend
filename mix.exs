427f8a51246acd55e61b71cbf42bc46936de6c27
Peter Gundel <mail@petergundel.de>
2023-02-27 20:52:39 +0100

    Fix Ash setup

--- mix.exs
+++ mix.exs
@@ -54,7 +54,7 @@ defmodule PeakTracker.MixProject do
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup"],
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.setup": ["ash_postgres.create", "ash_postgres.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
     ]
