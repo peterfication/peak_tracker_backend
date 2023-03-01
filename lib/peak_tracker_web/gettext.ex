b2b6ff3b4726af4dac617ccc71d1788bcbf603b6
Peter Gundel <mail@petergundel.de>
2023-03-01 20:34:05 +0100

    Fix some final test=>peak_tracker issues

--- lib/peak_tracker_web/gettext.ex
+++ lib/peak_tracker_web/gettext.ex
@@ -20,5 +20,5 @@ defmodule PeakTrackerWeb.Gettext do
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
   """
-  use Gettext, otp_app: :test
+  use Gettext, otp_app: :peak_tracker
 end
--- lib/peak_tracker_web/router.ex
+++ lib/peak_tracker_web/router.ex
@@ -34,7 +34,7 @@ defmodule PeakTrackerWeb.Router do
   end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
-  if Application.compile_env(:test, :dev_routes) do
+  if Application.compile_env(:peak_tracker, :dev_routes) do
     # If you want to use the LiveDashboard in production, you should put
     # it behind authentication and allow only admins to access it.
     # If your application does not have an admins-only section yet,
