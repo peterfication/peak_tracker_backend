344b705e8dcb038924d0fe6598b6f3b25a3673b0
Peter Gundel <mail@petergundel.de>
2023-03-02 06:58:42 +0100

    Seed multiple peaks

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,16 +1,24 @@
 # Script for populating the database.
 
-# Get a peak
-{status, peak} = PeakTracker.Mountains.get(PeakTracker.Mountains.Peak, slug: "zugspitze")
+peaks_data = [
+  %{name: "Zugspitze", slug: "zugspitze"},
+  %{name: "Mont Blanc", slug: "mont-blanc"},
+]
 
-if status == :ok do
-  # Update the peak
-  peak
-  |> Ash.Changeset.for_update(:update, %{name: "Zugspitze"})
-  |> PeakTracker.Mountains.update!()
-else
-  # Create the peak if it doesn't exist
-  PeakTracker.Mountains.Peak
-  |> Ash.Changeset.for_create(:create, %{name: "Zugspitze", slug: "zugspitze"})
-  |> PeakTracker.Mountains.create!()
+# loop through the peaks and create or update them
+for peak_data <- peaks_data do
+  # Get a peak
+  {status, peak} = PeakTracker.Mountains.get(PeakTracker.Mountains.Peak, slug: peak_data.slug)
+
+  if status == :ok do
+    # Update the peak if it exists
+    peak
+    |> Ash.Changeset.for_update(:update, %{name: peak_data.name})
+    |> PeakTracker.Mountains.update!()
+  else
+    # Create the peak if it doesn't exist
+    PeakTracker.Mountains.Peak
+    |> Ash.Changeset.for_create(:create, %{name: peak_data.name, slug: peak_data.slug})
+    |> PeakTracker.Mountains.create!()
+  end
 end
