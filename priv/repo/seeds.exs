9cb0b98d1e3437aaffb7468845d7782700a5f853
Peter Gundel <mail@petergundel.de>
2023-03-12 17:32:09 +0100

    Rewrite seeds to use case instead of if

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -4,15 +4,16 @@ for %{name: name, slug: slug} <- [
       %{name: "Zugspitze", slug: "zugspitze"},
       %{name: "Mont Blanc", slug: "mont-blanc"}
     ] do
-  {status, peak} = PeakTracker.Mountains.get(PeakTracker.Mountains.Peak, slug: slug)
+  # Create or update the peak
+  case PeakTracker.Mountains.get(PeakTracker.Mountains.Peak, slug: slug) do
+    {:ok, peak} ->
+      peak
+      |> Ash.Changeset.for_update(:update, %{name: name})
+      |> PeakTracker.Mountains.update!()
 
-  if status == :ok do
-    peak
-    |> Ash.Changeset.for_update(:update, %{name: name})
-    |> PeakTracker.Mountains.update!()
-  else
-    PeakTracker.Mountains.Peak
-    |> Ash.Changeset.for_create(:create, %{name: name, slug: slug})
-    |> PeakTracker.Mountains.create!()
+    {:error, _} ->
+      PeakTracker.Mountains.Peak
+      |> Ash.Changeset.for_create(:create, %{name: name, slug: slug})
+      |> PeakTracker.Mountains.create!()
   end
 end
