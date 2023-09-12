3d1f4f7bcaae5ea02bf9e811f1c16f7ad7ea11a6
Peter Morgenstern <mail@petergundel.de>
2023-09-12 12:51:23 +0200

    Add swapfile

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+fallocate -l 512M /swapfile
+chmod 0600 /swapfile
+mkswap /swapfile
+echo 10 > /proc/sys/vm/swappiness
+swapon /swapfile
+echo 1 > /proc/sys/vm/overcommit_memory
+
+exec su web /app/bin/server
